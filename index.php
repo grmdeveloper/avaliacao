f37f835a2a24aae052a10c8ee6b8e866bc219f36
grmdeveloper <machado.gr20@gmail.com>
2019-08-12 15:15:28 -0300

    f

--- index.php
+++ index.php
@@ -5,15 +5,13 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
-<script type="text/javascript">
-	doc
-</script>
 </head>
 <body>
-	<table class='table table-stripped'>
+	<table class='table table-stripped w-50' style='margin:auto;'>
 		<tr>
-			<img src="logo.jpg" width='55px' style='margin:10px'>
-			<th class='title h2 text-center'>
+			<th class='title h2 text-center' style='color:#a18635;'>
+			<img src="logo.jpg" width='120px' style='margin:10px;'>
+			<br>
 				Como foi seu atendimento?
 			</th>	
 		</tr>
@@ -21,8 +19,19 @@
 		<tr>
 			<td>
 			<form method='post' action='input.php' class='option'>	
+				<img src="excelente.png">
+				<input type="text" name="avaliacao" hidden value='EXCELENTE'>
+				<input type='submit' class='btn' value='EXCELENTE' id='excelente'>
+			</form>
+			</td>
+		</tr>
+			
+		<tr>
+			<td>
+			<form method='post' action='input.php' class='option'>	
+				<img src="bom.png">
 				<input type="text" name="avaliacao" hidden value='BOM'>
-				<input type='submit' class='btn btn-block' value='BOM' id='bom'>
+				<input type='submit' class='btn' value='BOM' id='bom'>
 			</form>
 			</td>
 		</tr>
@@ -30,16 +39,18 @@
 		<tr>
 			<td>	
 			<form method='post' action='input.php'  class='option'>
+				<img src="regular.png">
 				<input type="text" name="avaliacao" hidden value='REGULAR'>
-				<input type='submit' class='btn btn-block' value='REGULAR' id='regular'>
+				<input type='submit' class='btn' value='REGULAR' id='regular'>
 			</form>
 			</td>
 		</tr>
 		<tr>
 			<td>	
 			<form method='post' action='input.php' class='option'>	
+				<img src="ruim.png">
 				<input type="text" name="avaliacao" hidden value='RUIM'>
-				<input type='submit' class='btn btn-block' value='RUIM' id='ruim'>
+				<input type='submit' class='btn' value='RUIM' id='ruim'>
 			</form>
 			</td>
 		</tr>
@@ -47,24 +58,43 @@
 </body>
 
 <style type="text/css">
-	.option{
-		text-align:center;
-		color:white;
+	.table{
+		align-items:left;
 	}
+	.option{
+		color:white;
+		text-align: center;
+	}
+	#excelente{
+		background-color:#431887;
+		color:#eee;
+		width:250px;
+		font-size:21pt;
+		text-shadow:1px 1px 1px black;
+	}	
 	#bom{
 		background-color:green;
 		color:#eee;
+		width:250px;
+		font-size:21pt;
 		text-shadow:1px 1px 1px black;
 	}	
 	#regular{
 		background-color:orange;
 		color:#eee;
+		width:250px;
+		font-size:21pt;
 		text-shadow:1px 1px 1px black;
 	}	
 	#ruim{
 		background-color:red;
 		color:#eee;
+		width:250px;
+		font-size:21pt;
 		text-shadow:1px 1px 1px black;
+	}
+	form img{
+		width:60px;
 	}
 </style>
 </html>
