6f1fac51a53bee293e96f15408e8a5a208a354c5
grmdeveloper <machado.gr20@gmail.com>
2019-08-15 14:12:34 -0300

    v.1.0.1

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
 			<th class='title h1 text-center' style='color:#a18635;'>
 			<img src="logo.jpg" width='120px' style='margin:10px;'>
 			<br>
-				Como foi seu atendimento
-				<br> na clinica da família Walter Borges?
+				Como foi seu atendimento? 
 			</th>	
 		</tr>
 			
