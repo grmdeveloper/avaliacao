a5028e3b9051ec6c765d176c3fc7007e925b10d7
grmdeveloper <machado.gr20@gmail.com>
2019-08-16 16:43:02 -0300

    v2.0.12

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 	header('Content-type:text/html; charset=utf-8');
 	clearstatcache();
+	header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+	header("Pragma: no-cache"); // HTTP 1.0.
+	header("Expires: 0"); // Proxies.
 
 	require"connection.php";
 
